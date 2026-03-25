e802a155c82bb22853e2861e70cbf978bd7164e6
Benjamin Wagner <benjamin-wagner@mein.gmx>
2026-03-25 11:28:19 +0100

    chore: Update commitlint configuration to disable header and subject length checks

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,8 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "body-max-line-length": [0],
+    "header-max-length": [0],
     "subject-case": [0],
+    "subject-max-length": [0],
   },
 };
